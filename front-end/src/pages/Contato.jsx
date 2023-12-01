6efb6a22a9b0cc3a84baf6f987088ff5c9a4335c
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-12-01 07:02:47 -0300

    Arquivos formatados

--- front-end/src/pages/Contato.jsx
+++ front-end/src/pages/Contato.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { Card, Typography } from 'antd';
+import { Card, Typography } from "antd";
 import {
-  WhatsAppOutlined, MailOutlined , GithubOutlined
+  WhatsAppOutlined,
+  MailOutlined,
+  GithubOutlined,
 } from "@ant-design/icons";
 
 const { Title } = Typography;
 
 const App: React.FC = () => (
-  <div style={{marginTop: 120}}>
-  <Card   style={{ width: 400, fontSize: 20}}>
-    <Card>
-    <Title>Contato</Title>
+  <div style={{ marginTop: 120 }}>
+    <Card style={{ width: 400, fontSize: 20 }}>
+      <Card>
+        <Title>Contato</Title>
+      </Card>
+      <br />
+      <p>
+        <WhatsAppOutlined /> (48)988612552
+      </p>
+
+      <p>
+        <MailOutlined /> brunomozer05@gmail.com
+      </p>
+
+      <a href="https://github.com/brunomozer05">
+        <GithubOutlined /> brunomozer05
+      </a>
     </Card>
-    <br/>
-    <p ><WhatsAppOutlined/> (48)988612552</p>
-
-    <p><MailOutlined /> brunomozer05@gmail.com</p>
-
-    <a href="https://github.com/brunomozer05"><GithubOutlined /> brunomozer05</a>
-  </Card>
   </div>
 );
 
